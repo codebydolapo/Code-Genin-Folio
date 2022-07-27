c2579eb752050bcefd770c934f088c0bb23a5472
Bashorun Dolapo O <bashorun115@gmail.com>
2022-07-28 00:43:47 +0100

    fixed missing links

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23903c06f63f5a43"; }
+/******/ 		__webpack_require__.h = function() { return "2a2c1a2a311fc027"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,18 +1,34 @@
-import styles from '../styles/menu.module.css'
-import {useSelector} from 'react-redux'
+import styles from "../styles/menu.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
+function Menu() {
+  const menuState = useSelector((state: any) => state.menuState);
 
-function Menu(){
-
-    const menuState= useSelector((state: any)=> state.menuState)
-
-    return(
-        <div className = {menuState ? styles.menuActive : styles.menu}>
-            <p>Blog</p>
-            <p>Resume</p>
-            <p>Contact</p>
-        </div>
-    )
+  return (
+    <div className={menuState ? styles.menuActive : styles.menu}>
+      <p
+        onClick={() =>
+          window.open("https://blog.mohammedadekunle.com.ng/", "_blank")
+        }
+      >
+        Blog
+      </p>
+      <p
+        onClick={() =>
+          window.open(
+            "https://mohammedadekunle.com.ng/assets/mohammed-adekunle-resume.pdf",
+            "_blank"
+          )
+        }
+      >
+        Resume
+      </p>
+      <Link href="/#about">
+        <p>About</p>
+      </Link>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
